1228bf220480237c8117555e0cb5487a2a1a811f
zhouhui <1067072759@qq.com>
2018-01-16 17:58:03 +0800

    轮播图插件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // import BScroll from 'better-scroll'
+  import {addClass} from 'common/js/dom'
+  export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth () {
+        this.children = this.$refs.sliderGroup.children
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop) {
+          width += 2 * sliderWidth
+        }
+
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider () {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+  .slider
+    min-height 1px
+    .slider-group
+      position relative
+      white-space nowrap
+      overflow hidden
+      .slider-item
+        float left
+        box-sizing border-box
+        text-align center
+        overflow hidden
+        a
+          overflow hidden
+          width 100%
+          text-decoration none
+          display block
+          img
+            display block
+            width 100%
+</style>
